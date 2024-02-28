fec90d5f7164b01bc753582679dd8bbf6e8facb6
Dominik Gałenza <dominik.galenza@gmail.com>
2024-02-28 17:46:49 +0100

    Added ability to spawn actors using server RPC.

--- Source/Multiplayer/MultiplayerCharacter.cpp
+++ Source/Multiplayer/MultiplayerCharacter.cpp
@@ -10,6 +10,8 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/StaticMeshActor.h"
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
@@ -126,5 +128,36 @@ void AMultiplayerCharacter::Look(const FInputActionValue& Value)
 		// add yaw and pitch input to controller
 		AddControllerYawInput(LookAxisVector.X);
 		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void AMultiplayerCharacter::ServerRPCFunction_Implementation()
+{
+	if (HasAuthority())
+	{
+#if 0
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Server: ServerRPCFunction_Implementation"));
+#endif
+		if (!SphereMesh)
+		{
+			return;
+		}
+
+		AStaticMeshActor* StaticMeshActor = GetWorld()->SpawnActor<AStaticMeshActor>(AStaticMeshActor::StaticClass());
+		if (StaticMeshActor)
+		{
+			StaticMeshActor->SetReplicates(true);
+			StaticMeshActor->SetReplicateMovement(true);
+			StaticMeshActor->SetMobility(EComponentMobility::Movable);
+			FVector SpawnLocation = GetActorLocation() + GetActorRotation().Vector() * 100.0f + GetActorUpVector() * 50.0f;
+			StaticMeshActor->SetActorLocation(SpawnLocation);
+			UStaticMeshComponent* StaticMeshComponent = StaticMeshActor->GetStaticMeshComponent();
+			if (StaticMeshComponent)
+			{
+				StaticMeshComponent->SetIsReplicated(true);
+				StaticMeshComponent->SetSimulatePhysics(true);
+				StaticMeshComponent->SetStaticMesh(SphereMesh);
+			}
+		}
 	}
 }
